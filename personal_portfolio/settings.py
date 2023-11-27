dbe40cd9325bb915c48c4749c85b654e4e03245d
grayglow <grayglow@yandex.by>
2023-11-27 12:39:21 +0300

    ';

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -122,3 +122,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 MEDIA_URL = '/media/'
+
+
+try:
+    from .local_settings import *
+except ImportError:
+    print('looks like no local files')
